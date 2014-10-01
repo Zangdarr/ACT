1ea8dfe3ad1f46a9315ccb656c6e31233ac5dea9
Verkyndt Alexandre <alexandre.verkyndt@gmail.com>
2014-10-01 15:16:52 +0200

    Update TP2_Algorithme.java
    ajout des reponse Q4 Q5.1 Q6_v1

--- TP2_Algorithme.java
+++ TP2_Algorithme.java
@@ -1,19 +1,33 @@
 
 public class Algorithme {
     
-    /*cet algo est en n², il faut du teta(n)*/
-    public String Algo_Q4(int[] ref_list, int[] listA){
+   
+    public int[] Algo_Q4(int[] ref_list, int[] listA){
+	int[] tmp;
+	//inversion des indices et des valeur de la ref_list dans tmp 
+	for (int i=0; i<ref_list.length; i++){
+	    tmp[ref_list[i]] = i;
+	}
 	//normalisation de la listA
-	for(int i =0;i<ref_list.lenght;i++){
-	    for(int j=0; j<listA.lenght;j++){
-		if(ref_list[i] = listA[j]){
-		    listA[j] = i;
-		}
-	    }
+	for(int j=0; j<listA.length;j++){
+	    listA[j] = tmp[listA[j]]; 
 	}
-	//normalisation de la ref_list
-	for(int i =0;i<ref_list.lenght;i++){
-	    ref_list[i] = i;
-	}
+	return listA;
     }
+
+    public int Algo_Q5_1(int[] y){
+	int cpt_inversion = 0;
+	for(int i =0; i<y.length-1;i++)
+	    for(int j=i+1; j<y.length;j++)
+		if(y[i] > y[j])
+		    cpt_inversion++;
+	
+	return cpt_inversion;
+    }
+
+    public int Algo_Q6_v1(int[] ref_list, int[] listA){
+	return Algo_Q5_1(Algo_Q4(ref_list,listA));
+    }
+
+     
 }
